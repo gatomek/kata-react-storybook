4a2cb75e3bc5f83986f1d6638558bbb41be08f62
Gatomek <tomasz.galinski@gmail.com>
2026-05-14 20:51:43 +0200

    cr

--- src/components/CustomHeader/CustomHeader.types.ts
+++ src/components/CustomHeader/CustomHeader.types.ts
@@ -1,5 +1,5 @@
 import type {HTMLAttributes, ReactNode} from "react";
 
-export interface CustomHeaderProps extends HTMLAttributes<HTMLHeadingElement>{
+export interface CustomHeaderProps extends HTMLAttributes<HTMLElement>{
     children: ReactNode;
 }
